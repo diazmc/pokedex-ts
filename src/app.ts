cd15938d8a7f9900b3a351b2531f48b28a808adb
diazmc <mc.arthur_d@hotmail.com>
2020-06-06 21:44:29 -0700

    displays pokemons

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ const container: HTMLElement | any = document.getElementById("app")
 const pokemons: number = 100
 
 interface IPokemon {
-  id: number,
-  name: string,
-  image: string,
-  type: string
+  id: number;
+  name: string;
+  image: string;
+  type: string;
 }
 
 const fetchData = (): void => {
@@ -18,8 +18,8 @@ const getPokemon = async (id: number): Promise<void> => {
   const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
   const pokemon: any = await data.json()
   const pokemonType: string = pokemon.types
-  .map((poke: any) => poke.type.name)
-  .join(", ")
+    .map((poke: any) => poke.type.name)
+    .join(", ")
 
   const transformedPokemon = {
     id: pokemon.id,
@@ -31,15 +31,15 @@ const getPokemon = async (id: number): Promise<void> => {
   showPokemon(transformedPokemon)
 }
 
-const showPokemon = (pokemon: Ipokemon): void => {
+const showPokemon = (pokemon: IPokemon): void => {
   let output: string = `
         <div class="card">
-          <span class="card--id">#${pokemon.id}</span>
-          <img class="card--image" src=${pokemon.image} alt=${pokemon.name} />
-          <h1 class="card--name">${pokemon.name}</h1>
-          <span class="card--details">${pokemon.type}</span>
-  `
-
+            <span class="card--id">#${pokemon.id}</span>
+            <img class="card--image" src=${pokemon.image} alt=${pokemon.name} />
+            <h1 class="card--name">${pokemon.name}</h1>
+            <span class="card--details">${pokemon.type}</span>
+        </div>
+    `
   container.innerHTML += output
 }
 
